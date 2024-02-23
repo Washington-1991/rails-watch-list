791c86cc81ee787271bda7d8474bd6ab8de481d7
Washington Arambillete <milanghdarkoh@hotmail.com>
2024-02-23 15:28:19 +0100

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
 
   resources :lists
-  resources :bookmarks
+
 
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "pages#home"
