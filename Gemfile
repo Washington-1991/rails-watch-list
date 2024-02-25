b936798137443bae51156bf50478880b0e7a970d
Washington Arambillete <milanghdarkoh@hotmail.com>
2024-02-25 14:06:44 +0100

    Add dotenv-rails

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem "font-awesome-sass", "~> 6.1"
 gem "simple_form"
 gem "sassc-rails"
 gem 'faker'
-
+gem "dotenv-rails"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.1"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-
-  resources :lists
-
-
   get "up" => "rails/health#show", as: :rails_health_check
-  root to: "pages#home"
+
+  get "home" => "pages#home", as: :home
+  resources :lists, only: [:new, :create, :index, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 
   # Defines the root path route ("/")
   # root "posts#index"
